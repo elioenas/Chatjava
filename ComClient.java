140d740c076ebdb47394ed6af12b4a17af3687fa
Elioenas Batista de Sousa <30697363+elioenas@users.noreply.github.com>
2018-12-11 11:59:00 -0300

    Delete ComClient.java

--- ComClient.java
+++
@@ -1,9 +0,0 @@
-package Chat;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ComClient extends Remote {
-
-	void receiveMessage(String string) throws RemoteException;
-}
